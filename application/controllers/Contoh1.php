175d2f2d0a115b71e1abc456f961367200b56265
emiwulandari23 <emiulandaripon11@gmail.com>
2024-04-01 09:32:57 +0700

    pertemuan 3

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,9 +1,9 @@
 <?php
-class Tugas1 extends Tugas1_controller
+class Contoh1 extends CI_controller
 {
     public function index()
     {
-        echo "<h1>Perkenaalkan";
+        echo "<h1>Perkenalkan</h1>";
         echo "Nama saya Emi Wulandari, saya tinggal di Kecamatan Sungai Raya kegiatan yang saya lakukan adalah Scrol TikTok";
     }
 }
